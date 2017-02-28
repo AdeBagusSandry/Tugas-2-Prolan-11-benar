7c0bbe33a81536010fd6266dbc750c71a3a79670
AdeBagusSandry <adebagussandry@gmail.com>
2017-02-28 23:25:08 +0700

    Add files via upload

---
+++ Mahasiswa.java
@@ -0,0 +1,14 @@
+<html>
+<head>
+<title>Latihan Java Script </title>
+<h4><u><center><font face="Freshman " size="+2" color ="#13085b">Biodata Mahasiswa</font></u></h4></center>
+<body><pre><script language ="javascript">
+document.write("Nama Lengkap                : Ade Bagus Sandry </br>");
+document.write("NIM                         : 10114043 </br>" );
+document.write("Alamat                      : jl.Ir juanda dago pojok  No.28 </br>");
+
+
+</script>
+</body>
+</head>
+</html>
